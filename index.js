59300f195eb1b7cbca71228587d41d036124316c
BarathrajE <barathelumalai007@gmail.com>
2025-03-13 17:59:04 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,48 @@
+/*==============let================*/
+
+/*--------it`s possible---------*/
+
+/*
+let name="barath";
+name="change";
+console.log(name); //change
+
+
+--------------- it`s not possible------------
+
+let name="barath";
+let name="change"
+
+console.log(name);
+
+//in this SyntaxError
+
+
+
+/*===========const=================*/
+
+/*--------it`s not possible---------*/
+// const name="barath";
+// const name="change"
+// console.log(name); syntaxError
+
+
+/*--------it`s possible---------*/
+/*
+const name="barath";
+const fname=name;
+console.log(fname); //change
+
+*/
+// strings
+//let a='6';
+//let b="3";
+//console.log(a / b ); // 3, strings are converted to numbers
+
+// let age = Number("an arbitrary string instead of a number");
+
+// console.log(age); // NaN, conversion failed
+
+// Value	                         Becomes…
+// 0, null,  undefined, NaN, ""	     false
+// any other value	                 true
